5f609861fda77cbc377653b6aa29699ea77e84b9
TheKillerBunny <bunny@tkbunny.net>
2026-03-03 10:12:28 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const urlParams = new URLSearchParams(window.location);
+
+let size = 20;
+let count = 1;
+let mod = 0;
+
+for (const [key, val] of urlParams) {
+  switch(key) {
+    case "size":
+      size = parseInt(val);
+    case "count":
+      count = parseInt(val);
+    case "mod":
+      mod = parseInt(val);
+  }
+}
+
+let result = mod;
+let results = "";
+while (count > 0) {
+  count--;
+  
+  let roll = Math.floor(Math.random() * size) + 1
+  result += roll;
+  
+  if (results != "") {
+    results += ", "
+  }
+  
+  results += roll.toString()
+}
+
+document.getElementById("rolls").innerText = results.toString();
+document.getElementById("result").innerText = result.toString();
